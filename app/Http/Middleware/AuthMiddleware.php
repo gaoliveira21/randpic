3ded4f4eb561e154237ec337615bc68f5f55bd29
Gabriel Jose de Oliveira <gabriel.oliveira@liveonsolutions.com>
2020-06-20 19:33:27 -0300

    RAND-02/:rocket: AuthMiddleware created

---
+++ app/Http/Middleware/AuthMiddleware.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use JWTAuth;
+use Exception;
+use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
+
+class AuthMiddleware extends BaseMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+            $request->attributes->add(['currentUser' => $user]);
+        } catch(Exception $e) {
+            if($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
+                return response()->json(['status' => 'Token is invalid'], 401);
+            } else if($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+                return response()->json(['status' => 'Token is expired'], 401);
+            } else {
+                return response()->json(['status' => 'Authorization Token not found'], 401);
+            }
+        }
+
+        return $next($request);
+    }
+}
