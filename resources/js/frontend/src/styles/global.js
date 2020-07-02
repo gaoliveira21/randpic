b918180b728c3c4b63478ebb4e5bb52e1414b9e2
Pedro Lucas Bezerra <pedro.lucas210600@gmail.com>
2020-07-02 14:55:36 -0300

    ImageList page

--- resources/js/frontend/src/styles/global.js
+++ resources/js/frontend/src/styles/global.js
@@ -27,9 +27,13 @@ export default createGlobalStyle`
   }
   h1, h2, h3, h4, h5, h6 {
     font-family: Ubuntu, Arial, Helvetica, sans-serif;
+    color: #362C2B;
   }
   h1 {
     font-size: 32px;
+  }
+  p {
+    color: #333333;
   }
   a {
     text-decoration: none;
@@ -45,5 +49,37 @@ export default createGlobalStyle`
   img {
     width: 100%;
   }
-  
+
+  /* menu */
+  .menu {
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #fff;
+  padding: 24px 24px;
+  display: relative;
+}
+.menu h1{
+  float: left;
+}
+.menu ul{
+  float: right;
+}
+.menu ul li{
+  display: inline;
+  padding: 24px 18px;
+}
+.menu ul li a{
+  font-size: 16px;
+  color: #555555;
+}
+.menu ul li button{
+  background-color: #A26769;
+  padding: 12px 24px;
+  border: none;
+  border-radius: 4px;
+}
+.menu ul li button a{
+  color: #fff;
+}
 `;
