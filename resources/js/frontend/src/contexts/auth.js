85973a9a199654b43c244282f8ec863226c84e5d
Gabriel Jose de Oliveira <gabriel.oliveira@liveonsolutions.com>
2020-07-06 22:01:30 -0300

    profile/edit user profile

--- resources/js/frontend/src/contexts/auth.js
+++ resources/js/frontend/src/contexts/auth.js
@@ -8,6 +8,12 @@ const AuthContext = createContext({});
 export function AuthProvider({ children }) {
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('@Randpic:user')));
 
+    useEffect(() => {
+        const storagedToken = localStorage.getItem('@Randpic:token');
+        if (storagedToken)
+            api.defaults.headers.Authorization = `Bearer ${storagedToken}`;
+    }, []);
+
     async function signIn({ email, password }) {
         try {
             const response = await api.post('/auth', { email, password });
@@ -22,7 +28,7 @@ export function AuthProvider({ children }) {
             setUser(user);
             return true;
         } catch (error) {
-            toast.error('Usuario não encontrado, e-mail e senha incorretos');
+            toast.error('Fails on login, was not found a user with this email and password');
             return false;
         }
     }
@@ -35,15 +41,13 @@ export function AuthProvider({ children }) {
 
             api.defaults.headers.Authorization = `Bearer ${access_token}`;
 
-            console.log(user);
-
             localStorage.setItem('@Randpic:user', JSON.stringify(user));
             localStorage.setItem('@Randpic:token', access_token);
 
             setUser(user);
             return true;
         } catch (error) {
-            toast.error('Falha ao cadastrar usuário, tente novamente');
+            toast.error('Fails on create user');
             return false;
         }
     }
@@ -53,13 +57,30 @@ export function AuthProvider({ children }) {
         setUser(null);
     }
 
+    async function editProfile({ name, email }) {
+        try {
+            const response = await api.put('/users', { name, email });
+            const { updated_at, created_at, id} = response.data;
+
+            const user = { name, email, id, updated_at, created_at };
+
+            localStorage.setItem('@Randpic:user', JSON.stringify(user));
+            setUser(user);
+
+            toast.success('Success on update profile');
+        } catch (error) {
+            toast.error('Fails on update profile');
+        }
+    }
+
     return (
         <AuthContext.Provider value={{
             signed: !!user,
             user,
             signIn,
             signUp,
-            signOut
+            signOut,
+            editProfile
         }}>
             {children}
         </AuthContext.Provider>
--- resources/js/frontend/src/pages/User/index.js
+++ resources/js/frontend/src/pages/User/index.js
@@ -1,45 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
 import { FiEdit } from 'react-icons/fi';
+import { Form, Input } from '@rocketseat/unform';
+import * as yup from 'yup';
 
 import Header from '../../components/Header';
+import AuthContext from '../../contexts/auth';
 import './styles.css';
 
+const schema = yup.object().shape({
+    name: yup.string().required('O campo nome é obrigatório'),
+    email: yup.string().email('Endereço de e-mail inválido').required('O campo e-mail é obrigatório'),
+});
+
 function User() {
-  return (
-    <>
-      <Header></Header>
-      <main className="container-user">
-        <section>
-          <h1>Perfil</h1>
-          <form action="" className="form-user">
-            <div>
-              <label htmlFor="name">Name:</label>
-              <input type="text" name="name" id="name"/>
-            </div>
-            <div>
-              <label htmlFor="email">Email:</label>
-              <input type="email" name="email" id="email"/>
-            </div>
-            <div>
-              <label htmlFor="oldPassword">Old Password:</label>
-              <input type="password" name="oldPassword" id="oldPassword"/>
-            </div>
-            <div>
-              <label htmlFor="newPassword">New Password:</label>
-              <input type="password" name="newPassword" id="newPassword"/>
-            </div>
-            <div>
-              <label htmlFor="confirmPassword">Confirm Password:</label>
-              <input type="password" name="confirmPassword" id="confirmPassword"/>
-            </div>
-            <div className="button">
-              <button type="submit">Enviar<FiEdit/></button>
-            </div>
-          </form>
-        </section>
-      </main>
-    </>
-  );
+    const { user, editProfile } = useContext(AuthContext);
+
+    async function handleSubmit(data) {
+        editProfile(data);
+    }
+
+    return (
+        <>
+            <Header></Header>
+            <main className="container-user">
+                <section>
+                    <h1>Perfil</h1>
+                    <Form schema={schema} initialData={user} onSubmit={handleSubmit} className="form-user">
+                        <div>
+                            <label htmlFor="name">Name:</label>
+                            <Input type="text" name="name" id="name" />
+                        </div>
+                        <div>
+                            <label htmlFor="email">Email:</label>
+                            <Input type="email" name="email" id="email" />
+                        </div>
+                        <div className="button">
+                            <button type="submit">Enviar<FiEdit /></button>
+                        </div>
+                    </Form>
+                </section>
+            </main>
+        </>
+    );
 }
 export default User;
