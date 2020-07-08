6f060cf83a1d7e537f9b5ebe62856e62d51b247c
Gabriel Jose de Oliveira <gabriel.oliveira@liveonsolutions.com>
2020-07-07 21:01:32 -0300

    frontend/download image

--- resources/js/frontend/src/pages/ImageDownload/index.js
+++ resources/js/frontend/src/pages/ImageDownload/index.js
@@ -1,21 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import Switch from 'react-switch';
 import { FiArrowLeft, FiArrowDown } from 'react-icons/fi';
 import { FaHeart, FaRegHeart } from 'react-icons/fa';
+import FileDownload from 'js-file-download';
 
+import api from '../../services/api';
 import Header from '../../components/Header';
-import dog from '../../assets/dog.jpg';
 
 import './styles.css';
 
-function imageDownload() {
+function imageDownload({ location }) {
+
+    const [url, setUrl] = useState(location.state.url);
+    const [grayscale, setGrayscale] = useState(false);
     const [btnVisible, setBtnVisible] = useState(false);
     const [btnFavorite, setBtnFavorite] = useState(false);
+    const [download, setDownload] = useState('');
 
     const history = useHistory();
+
+    useEffect(() => {
+        grayscale ? setUrl(`${url}?grayscale`) : setUrl(location.state.url);
+    }, [grayscale])
+
+    useEffect(() => {
+        fetch(url).then(response => response.blob().then(blob => {
+            console.log(response);
+            const downloadUrl = window.URL.createObjectURL(blob);
+            setDownload(downloadUrl);
+        }));
+    }, [url])
+
     function backPage() {
         history.goBack();
+    }
+
+    async function handleDownload() {
+        console.log(download, url);
     }
 
     return (
@@ -28,7 +50,7 @@ function imageDownload() {
             </div>
             <main className="container-imageDownload">
                 <section className="content-imageDownload">
-                    <img src={dog} alt="DOG" />
+                    <img src={url} alt="DOG" />
                 </section>
 
                 <section className="content-tools">
@@ -36,8 +58,8 @@ function imageDownload() {
                     <div className="grayscale-filter">
                         <h4>Grayscale</h4>
                         <Switch
-                            onChange={() => { }}
-                            checked={false}
+                            onChange={() => setGrayscale(!grayscale)}
+                            checked={grayscale}
                             height={20}
                             width={50}
                             handleDiameter={25}
@@ -45,23 +67,7 @@ function imageDownload() {
                             onColor={'#D5B9B2'}
                             offHandleColor={'#555555'}
                             onHandleColor={'#A26769'}
-                        ></Switch>
-                    </div>
-                    <div className="blur-filter">
-                        <h4>Blur</h4>
-                        <select name="blur" id="blur">
-                            <option value="0">0</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
-                            <option value="7">7</option>
-                            <option value="8">8</option>
-                            <option value="9">9</option>
-                            <option value="10">10</option>
-                        </select>
+                        />
                     </div>
                     <div className="favorite">
                         {btnFavorite ?
@@ -101,8 +107,8 @@ function imageDownload() {
                                 <label htmlFor="little"><strong>Little </strong>(1280 x 960)</label>
                             </div>
                             <hr />
-                            <div class="btn-download">
-                                <button>Download Free</button>
+                            <div className="btn-download" onClick={handleDownload}>
+                                <a href={download} download>Download Free</a>
                             </div>
                         </div>
                     </div>
