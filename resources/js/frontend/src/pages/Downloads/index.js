d4b40a429ec665f15d7fe62ffad2560546b19a27
Gabriel Jose de Oliveira <gabriel.oliveira@liveonsolutions.com>
2020-07-07 22:09:06 -0300

    frontend/list downloads and format date

--- resources/js/frontend/src/pages/Downloads/index.js
+++ resources/js/frontend/src/pages/Downloads/index.js
@@ -1,39 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { format, subHours } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+// import { utcToZonedTime } from 'date-fns-tz';
 import { Link } from 'react-router-dom';
 
-import dog from '../../assets/dog.jpg';
+import api from '../../services/api';
 
+import AuthContext from '../../contexts/auth';
 import Header from '../../components/Header';
 import './styles.css';
 
 function Download() {
-  return (
-    <>
-      <Header></Header>
-      <main>
-        <section className="container-download">
-          <h1>Lista de Downloads</h1>
-          <table>
-            <thead>
-              <tr>
-                <th>Imagem</th>
-                <th>Date</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>
-                  <img src={dog} alt="Dog" />
-                </td>
-                <td>
-                  Data de download da imagem
-              </td>
-              </tr>
-            </tbody>
-          </table>
-        </section>
-      </main>
-    </>
-  );
+    const [downloads, setDownloads] = useState([]);
+    const { token } = useContext(AuthContext);
+
+    useEffect(() => {
+        async function loadDownloads() {
+            const response = await api.get('/downloads', {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+
+            const data = response.data.map(({ id, download_url, created_at }) => {
+                const date = new Date(created_at);
+
+                const subDate = subHours(date, 3);
+
+                return {
+                    id,
+                    download_url,
+                    created_at: format(subDate, "dd/MM/yyyy", { locale: pt })
+                }
+            })
+
+            setDownloads(data);
+        }
+        loadDownloads();
+    }, []);
+
+    return (
+        <>
+            <Header></Header>
+            <main>
+                <section className="container-download">
+                    <h1>Lista de Downloads</h1>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Imagem</th>
+                                <th>Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {downloads.map(download => (
+                                <tr key={download.id}>
+                                    <td>
+                                        <img src={download.download_url} />
+                                    </td>
+                                    <td>
+                                        <span>{download.created_at}</span>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </section>
+            </main>
+        </>
+    );
 }
 export default Download;
